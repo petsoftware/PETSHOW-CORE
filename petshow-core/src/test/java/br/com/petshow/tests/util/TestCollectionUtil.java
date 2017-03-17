550328fed7c7b6463c669f29d04317396546aa5b
antoniorafael <rafasystec@yahoo.com.br>
2017-03-17 12:46:11 -0300

    Enviando novas classes para as fotos

---
+++ petshow-core/src/test/java/br/com/petshow/tests/util/TestCollectionUtil.java
@@ -0,0 +1,32 @@
+package br.com.petshow.tests.util;
+
+import java.util.ArrayList;
+import java.util.List;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+import br.com.petshow.util.CollectionUtil;
+
+public class TestCollectionUtil {
+	@Test
+	public void testRemoveStringCollection() {
+		List<String> list = new ArrayList<>();
+		list.add("raimundo");
+		list.add("joão");
+		list.add("maria");
+		list.add("Jose");
+		list.add("Bruno");
+		
+		List<String> listClone = new ArrayList<>();
+		listClone.add("joão");
+		listClone.add("maria");
+		listClone.add("Jose");
+		listClone.add("Bruno");
+	
+		list = CollectionUtil.removeItem(list, "raimundo");
+		
+		assertEquals(listClone, list);
+		
+	}
+
+}
