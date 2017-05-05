837fe61e60a438380536bf4e2b62288bb0d39b7a
antoniorafael <rafasystec@yahoo.com.br>
2017-05-05 18:19:58 -0300

    Grupo de tabelas para o Spring Security

---
+++ petshow-core/src/main/java/br/com/petshow/model/Authorities.java
@@ -0,0 +1,15 @@
+package br.com.petshow.model;
+
+public class Authorities {
+	
+	/**
+	 * 
+	 *   create table authorities (
+      username varchar_ignorecase(50) not null,
+      authority varchar_ignorecase(50) not null,
+      constraint fk_authorities_users foreign key(username) references users(username));
+      create unique index ix_auth_username on authorities (username,authorit
+	 * 
+	 */
+
+}
---
+++ petshow-core/src/main/java/br/com/petshow/model/GroupAuthorities.java
@@ -0,0 +1,12 @@
+package br.com.petshow.model;
+
+public class GroupAuthorities {
+	/*
+	 * 
+	 * create table group_authorities (
+  group_id bigint not null,
+  authority varchar(50) not null,
+  constraint fk_group_authorities_group foreign key(group_id) references groups(id));
+	 */
+
+}
