b162b7c9b3d310c8d871f433ddd420afeb7f1ae5
antoniorafael <rafasystec@yahoo.com.br>
2017-06-07 18:04:15 -0300

    Incluir formatação de um java.util.Date na DateUtil

--- petshow-core/src/main/java/br/com/petshow/util/DateUtil.java
+++ petshow-core/src/main/java/br/com/petshow/util/DateUtil.java
@@ -1,18 +1,26 @@
 package br.com.petshow.util;
 
 import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 
 public class DateUtil {
 
-
+	/**
+	 * FORMATO dd/MM/yyyy
+	 */
+	public static final String  DD_MM_YYYY = "dd/MM/yyyy";
+	/**
+	 * FORMATO ddMMyyyy
+	 */
+	public static final String  DDMMYYYY = "ddMMyyyy";
+	/**
+	 * FORMATO yyyyMMdd
+	 */
+	public static final String  YYYYMMDD = "yyyyMMdd";
+	
 	public static Date addDays(Date date,int days){
-
-
 		Calendar c = Calendar.getInstance(); 
 		c.setTime(date); 
 		c.add(Calendar.DAY_OF_MONTH,days);
@@ -21,13 +29,10 @@ public class DateUtil {
 	}
 
 	public static String dateTo_ddMMYYYY(Date date){
-
 		return new SimpleDateFormat("dd/MM/yyyy").format(date);
 	}
 
 	public static Date addMonths(Date date,int months){
-
-
 		int days = months*30;
 		Calendar c = Calendar.getInstance(); 
 		c.setTime(date); 
@@ -36,4 +41,13 @@ public class DateUtil {
 		return date;
 	}
 
+	public static String formatar(Date data, String formatoSaida)
+	{
+		if(data == null)
+			return "";
+		Locale locale = new Locale("pt","br");
+		SimpleDateFormat dataFormato = new SimpleDateFormat (formatoSaida, locale);   
+		String dataFormatada = dataFormato.format(data);
+		return dataFormatada;
+	}
 }
