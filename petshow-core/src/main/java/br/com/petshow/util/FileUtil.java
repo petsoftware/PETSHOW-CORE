92519035e8f7aaa5481e0680da9b95497d329101
Rafael Rocha <rafasystec@yahoo.com.br>
2020-05-15 11:21:21 -0300

    new change

--- petshow-core/src/main/java/br/com/petshow/util/FileUtil.java
+++ petshow-core/src/main/java/br/com/petshow/util/FileUtil.java
@@ -1,9 +1,9 @@
 package br.com.petshow.util;
 
 import java.io.File;
-import java.io.IOException;
-import br.com.petshow.model.Entidade;
-import br.com.petshow.role.RoleParametros;
+//import java.io.IOException;
+//import br.com.petshow.model.Entidade;
+//import br.com.petshow.role.RoleParametros;
 
 public class FileUtil {
 	
@@ -20,33 +20,33 @@ public class FileUtil {
 		return file;
 	}
 
-	public static File checkAndCreateTheImagesServerRepository(Entidade entidade) {
-		String pathname = getPathName(entidade);
-		File file = new File(RoleParametros.paramRepoFiles+pathname);
-		if(!file.exists()){
-			file.mkdirs();
-		}
-		return file;
-	}
+//	public static File checkAndCreateTheImagesServerRepository(Entidade entidade) {
+//		String pathname = getPathName(entidade);
+//		File file = new File(RoleParametros.paramRepoFiles+pathname);
+//		if(!file.exists()){
+//			file.mkdirs();
+//		}
+//		return file;
+//	}
 
-	private static String getPathName(Entidade entidade) {
-		String pathname = File.separator+entidade.getClass().getTypeName().replace("br.com.petshow.model.", "")+File.separator+entidade.getId();
-		return pathname;
-	}
+//	private static String getPathName(Entidade entidade) {
+//		String pathname = File.separator+entidade.getClass().getTypeName().replace("br.com.petshow.model.", "")+File.separator+entidade.getId();
+//		return pathname;
+//	}
 	
-	public static String saveCertificateOnFileSystem(Entidade entidade,String imgBase64) {
-		if(imgBase64 != null && imgBase64.trim().isEmpty() == false){
-			try {
-				File file = checkAndCreateTheImagesServerRepository(entidade);
-				String fileName = HelperUniqueID.get()+".jpg";
-				File fileImage = new File(file.getAbsolutePath()+File.separator+fileName);
-				HelperBase64.toFile(imgBase64, fileImage);
-				return RoleParametros.paramSiteBaseUrl+VariableUtil.docBaseRepoFiles +getPathName(entidade).replaceAll(File.separator, "/")+"/"+fileName;
-			} catch (IOException e) {
-				e.printStackTrace();
-				return "";
-			}
-		}
-		return "";
-	}
+//	public static String saveCertificateOnFileSystem(Entidade entidade,String imgBase64) {
+//		if(imgBase64 != null && imgBase64.trim().isEmpty() == false){
+//			try {
+//				File file = checkAndCreateTheImagesServerRepository(entidade);
+//				String fileName = HelperUniqueID.get()+".jpg";
+//				File fileImage = new File(file.getAbsolutePath()+File.separator+fileName);
+//				HelperBase64.toFile(imgBase64, fileImage);
+//				return RoleParametros.paramSiteBaseUrl+VariableUtil.docBaseRepoFiles +getPathName(entidade).replaceAll(File.separator, "/")+"/"+fileName;
+//			} catch (IOException e) {
+//				e.printStackTrace();
+//				return "";
+//			}
+//		}
+//		return "";
+//	}
 }
